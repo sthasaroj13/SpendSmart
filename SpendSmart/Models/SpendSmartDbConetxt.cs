79489c534049880b5757e9145bc389f66fa16ffc
sthasaroj13 <sarojshrestha651@gmail.com>
2024-11-28 14:04:06 +0545

    Add project files.

---
+++ SpendSmart/Models/SpendSmartDbConetxt.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbConetxt:DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public SpendSmartDbConetxt(DbContextOptions<SpendSmartDbConetxt> options) :base(options) 
+        {
+            
+        }
+
+    }
+}
